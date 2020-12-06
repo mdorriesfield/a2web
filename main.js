24a2f742692d2118f3c3c07ece3399eb44e20f92
kgillingham <kazuto.gillingham@flemingcollege.ca>
2020-12-06 17:40:40 -0500

    Created main.js, removed dropMenu function code from index.html

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+// file created by Kazuto
 
+// function created by Kate
+function dropMenu() {
+  
+    var x = document.getElementById("SmallNavBar");
+    if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+    } 
+    else { 
+      x.className = x.className.replace("w3-show", "");
+    }
+  }
