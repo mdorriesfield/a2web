9ac392ed45dcc3da6540933bc70096f1765ca5d5
kathrynlflemingc <kathryn.little@flemingcollege.ca>
2020-12-06 22:38:25 -0500

    mouseover function on image

--- main.js
+++ main.js
@@ -13,13 +13,13 @@ function dropMenu() {
   }
 
 
-/* following code inspired by: https://stackoverflow.com/questions/54853648/on-mouse-hover-change-picture-in-javascript */
-function imageChange1() {
-    var image = document.getElementById("image1");
+// /* following code inspired by: https://stackoverflow.com/questions/54853648/on-mouse-hover-change-picture-in-javascript */
+// function imageChange1() {
+//     var image = document.getElementById("image1");
     
-    image.addEventListener('mouseover', function(){
-      image.src = "images/surveyplan_400_252.jpg"
-    })
-    image.addEventListener('mouseout', function(){
-        image.src = "images/cartography_400_237.jpg"
-    })
+//     image.addEventListener('mouseover', function(){
+//       image.src = "images/surveyplan_400_252.jpg"
+//     })
+//     image.addEventListener('mouseout', function(){
+//         image.src = "images/cartography_400_237.jpg"
+//     })
