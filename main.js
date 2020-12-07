fb9088d787cce78d0a55d73c11c88dec50ef76ad
kathrynlflemingc <kathryn.little@flemingcollege.ca>
2020-12-06 20:09:22 -0500

    edit to js file

--- main.js
+++ main.js
@@ -4,10 +4,10 @@
 function dropMenu() {
   
     var x = document.getElementById("SmallNavBar");
-    if (x.className.indexOf("w3-show") == -1) {
-      x.className += " w3-show";
+    if (x.className.indexOf("show") == -1) {
+      x.className += " show";
     } 
     else { 
-      x.className = x.className.replace("w3-show", "");
+      x.className = x.className.replace("show", "");
     }
   }
