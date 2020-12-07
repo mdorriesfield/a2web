86c21393e5baea0bdbde272a532d8709da2cb035
kathrynlflemingc <kathryn.little@flemingcollege.ca>
2020-12-06 22:29:49 -0500

    javascript function for images

--- main.js
+++ main.js
@@ -11,3 +11,13 @@ function dropMenu() {
       x.className = x.className.replace("show", "");
     }
   }
+
+function imageChange1() {
+    var image = document.getElementById("image1");
+    
+    image.addEventListener('mouseover', function(){
+      image.src = "images/surveyplan_400_252.jpg"
+    })
+    image.addEventListener('mouseout', function(){
+        image.src = "images/cartography_400_237.jpg"
+    })
